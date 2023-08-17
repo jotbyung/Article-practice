15b7d6565058e9c6358c6e8ba569111e1325dac6
jotbyung <pyhon7410@gmail.com>
2023-08-17 12:44:31 +0900

    게시물 상세 및 삭제 기능 구현 (복습)

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
-    SimpleDateFormat format1 = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss");
+    SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     Date time = new Date();
     String time1 = format1.format(time);
 
@@ -18,13 +18,12 @@ class Main {
     List<Article> articles = new ArrayList<>();
 
 
-
     while (true) {
       System.out.printf("명령어 )");
       String cmd = sc.nextLine().trim();
 
 
-      if (cmd.length()==0) {
+      if (cmd.length() == 0) {
         System.out.println("명령어를 입력하세요");
         continue;
       }
@@ -41,23 +40,21 @@ class Main {
         String content = sc.nextLine();
 
 
-        Article article = new Article(id,title,content);
+        Article article = new Article(id, title, content);
         articles.add(article);
 
 
-        System.out.printf("%d번 글이 생성되었습니다.\n",id);
-      }
-      else if (cmd.equals("article list")) {
+        System.out.printf("%d번 글이 생성되었습니다.\n", id);
+      } else if (cmd.equals("article list")) {
         if (articles.size() == 0) {
           System.out.println("게시글이 없습니다.");
         } else {
-          for (int i = articles.size()-1; i >= 0; i--) {
+          for (int i = articles.size() - 1; i >= 0; i--) {
             Article article = articles.get(i);
-            System.out.printf("%d  |  %s\n",article.id, article.title);
+            System.out.printf("%d  |  %s\n", article.id, article.title);
           }
         }
-      }
-      else if (cmd.startsWith("article detail")) {
+      } else if (cmd.startsWith("article detail")) {
         String[] cmdBits = cmd.split(" ");
         int id = Integer.parseInt(cmdBits[2]);
 
@@ -71,7 +68,7 @@ class Main {
           }
         }
         if (foundArticle == null) {
-          System.out.printf("%d번 글이 없습니다.\n",id);
+          System.out.printf("%d번 글이 없습니다.\n", id);
           continue;
         } else {
           System.out.printf("번호 : %d\n", id);
@@ -79,25 +76,48 @@ class Main {
           System.out.printf("제목 : %s\n", foundArticle.title);
           System.out.printf("내용 : %s\n", foundArticle.content);
         }
-      }
-      else if (cmd.startsWith("article delete")) {
+      } else if (cmd.startsWith("article delete")) {
         String[] cmdBits = cmd.split(" ");
         int id = Integer.parseInt(cmdBits[2]);
 
+//        int foundIdx = -1;
+//
+//
+//        for (int i = 0; i < articles.size(); i++) {
+//          Article article = articles.get(i);
+//          if (article.id == id) {
+//            foundIdx = i;
+//            break;
+//          }
+//        }
+//
+//        if (foundIdx == -1) {
+//          System.out.printf("%d번 글이 없습니다.\n", id);
+//          continue;
+//        }
+//
+//        articles.remove(id-1);
+//        System.out.printf("%d번 글이 삭제되었습니다.\n", id);
+//        3개 글이 존재할때, 3->1->2 순으로 삭제하면 오류가 뜸
 
+        Article foundArticle = null;
         for (int i = 0; i < articles.size(); i++) {
           Article article = articles.get(i);
           if (article.id == id) {
+            foundArticle = article;
             articles.remove(i);
-            System.out.printf("%d번 글이 삭제되었습니다.\n",article.id);
+            System.out.printf("%d번 게시글이 삭제되었습니다.\n",id);
             break;
           }
-          else {
-            System.out.printf("%d번 글이 없습니다.\n",id);
-            continue;
-          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+          continue;
         }
       }
+
+
       else {
         System.out.println("존재하지 않는 명령어입니다.");
       }
